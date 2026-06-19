49d63f3600ebf0407d5084b4a2dac64ebb4a9b13
Konnotakuto <s2522013@stu.musashino-u.ac.jp>
2026-06-19 15:51:05 +0900

    Initial Commit

---
+++ str(8) ToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  str(8) ToDo
+//
+//  Created by Konno Takuto on 2026/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ str(8) ToDo/str_8__ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  str_8__ToDoApp.swift
+//  str(8) ToDo
+//
+//  Created by Konno Takuto on 2026/06/19.
+//
+
+import SwiftUI
+
+@main
+struct str_8__ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
